639046a09d481174a2b543958e244cabe5f791bc
Pavel Kulagin <pavel.kulagin@corp.badoo.com>
2018-01-21 11:33:59 +0000

    placeholder now appears in the end of Gamezone's children

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler {
-	public delegate void Dragging();
+	public delegate void Dragging(String name);
 	public static event Dragging DraggingStarted;
 	public static event Dragging DraggingFinished;
 	private Parent _parent;
@@ -13,12 +12,13 @@ public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
 
 	public void OnBeginDrag(PointerEventData eventData)
 	{
+		if (DraggingStarted != null) DraggingStarted(name);
+		
 		_parent = new Parent(transform.parent);
 		transform.SetParent (transform.parent.parent);
 		_delta = new Vector2(transform.position.x - eventData.position.x, transform.position.y - eventData.position.y);
 		GetComponent<CanvasGroup> ().blocksRaycasts = false;
 
-		if (DraggingStarted != null) DraggingStarted();
 		Debug.Log ("Draggable " + name + " is listening to events");
 		DropZone.PointerEntered += _parent.SetPotential;
 		DropZone.PointerExited += _parent.Reset;
@@ -35,7 +35,7 @@ public class Draggable : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDra
 
 		GetComponent<CanvasGroup> ().blocksRaycasts = true;
 
-		if (DraggingFinished != null) DraggingFinished();
+		if (DraggingFinished != null) DraggingFinished(name);
 		Debug.Log ("Fuck this");
 		DropZone.PointerEntered -= _parent.SetPotential;
 		DropZone.PointerExited -= _parent.Reset;
--- Assets/Scripts/DropZone.cs
+++ Assets/Scripts/DropZone.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DropZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 	public delegate void Entered(Transform dropZone);
@@ -9,7 +11,9 @@ public class DropZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 	public delegate void Exited();
 	public static event Exited PointerExited;
 
-	private bool _draggingInProgress;
+	public Transform Card;
+	private Transform _placeholder;
+	private bool _dragginInProgress;
 
 	public void OnEnable(){
 		Draggable.DraggingStarted += EnableListening;
@@ -22,22 +26,37 @@ public class DropZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 	}
 
 	public void OnPointerEnter(PointerEventData eventData) {
-		if(_draggingInProgress && PointerEntered != null)
+		if(PointerEntered != null)
 			PointerEntered (transform);
+
+		if (_dragginInProgress)
+		{
+			_placeholder = Instantiate(Card);
+			for (int i = 0; i < _placeholder.childCount; i++)
+			{
+				Destroy(_placeholder.GetChild(i).gameObject);
+			}
+
+			Destroy(_placeholder.GetComponent<Image>());
+			_placeholder.SetParent(transform);
+		}
 	}
 
 	public void OnPointerExit(PointerEventData eventData) {
-		if(_draggingInProgress && PointerExited != null)
+		if(PointerExited != null)
 			PointerExited ();
+		if(_placeholder != null)
+			Destroy(_placeholder.gameObject);
 	}
 
-	private void EnableListening() {
-		Debug.Log ("Dropzone "+ this.name + " started to listen");
-		_draggingInProgress = true;
+	private void EnableListening(String name)
+	{
+		_dragginInProgress = true;
 	}
 
-	private void DisableListening() {
-		Debug.Log ("Dropzone "+ this.name + " stopped to listen");
-		_draggingInProgress = false;
+	private void DisableListening(String name) {
+		if(_placeholder != null)
+			Destroy(_placeholder.gameObject);
+		_dragginInProgress = false;
 	}
 }
